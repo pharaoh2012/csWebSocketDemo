01ea5e932d071bdf5293d127448a5a499f950671
raoming <raoming@dcqtech.com>
2017-01-14 16:19:37 +0800

    添加广播消息

--- csServer/csServer/EchoServer.cs
+++ csServer/csServer/EchoServer.cs
@@ -11,6 +11,7 @@ namespace csServer
         protected override void OnMessage(WebSocketSharp.MessageEventArgs e)
         {
             this.Send("服务端返回：" + e.Data);
+            this.Sessions.Broadcast(this.ID+ " say:" + e.Data );  //广播消息
             base.OnMessage(e);
         }
     }
